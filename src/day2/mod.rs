8871581d9c76ef84407a90cecdff8c99612bd0a3
moustapharamadan <moustapharamadan@hotmail.com>
2025-10-26 14:30:25 +0000

    Day2

---
+++ src/day2/mod.rs
@@ -0,0 +1,97 @@
+use anyhow::Result;
+use std::fs;
+
+fn parse_input(input: &str) -> Result<Vec<Vec<i32>>> {
+    let content = fs::read_to_string(input)?;
+
+    let mut result = Vec::new();
+    for row in content.lines() {
+        result.push(
+            row.split_whitespace()
+                .map(|x| x.parse::<i32>().expect("Expected integer"))
+                .collect::<Vec<i32>>(),
+        );
+    }
+
+    Ok(result)
+}
+
+fn is_safe(row: &Vec<i32>, recursive: bool) -> bool {
+    if row.len() < 2 {
+        return true;
+    }
+
+    let coef = if row[0] < row[1] {
+        -1
+    } else if row[0] > row[1] {
+        1
+    } else {
+        0
+    };
+
+    for i in 0..row.len() - 1 {
+        let diff = (row[i] - row[i + 1]) * coef;
+        let safe = 1 <= diff && diff <= 3;
+        if !safe && !recursive {
+            return false;
+        } else if !safe {
+            let mut row_without_i = row.clone();
+            row_without_i.remove(i);
+
+            let mut row_without_i_plus_1 = row.clone();
+            row_without_i_plus_1.remove(i + 1);
+
+            if is_safe(&row_without_i, false) || is_safe(&row_without_i_plus_1, false) {
+                return true;
+            }
+
+            if i >= 1 {
+                let mut row_without_i_min_1 = row.clone();
+                row_without_i_min_1.remove(i - 1);
+                if is_safe(&row_without_i_min_1, false) {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+    true
+}
+
+pub fn part1(input: &str) -> Result<u32> {
+    let input = parse_input(input)?;
+    Ok(input
+        .iter()
+        .map(|row| if is_safe(&row, false) { 1 } else { 0 })
+        .sum())
+}
+
+pub fn part2(input: &str) -> Result<u32> {
+    let input = parse_input(input)?;
+    Ok(input
+        .iter()
+        .map(|row| if is_safe(&row, true) { 1 } else { 0 })
+        .sum())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part1() {
+        let result = part1("data/example/day2.txt");
+
+        assert!(result.is_ok());
+        assert_eq!(result.unwrap(), 2);
+    }
+
+    #[test]
+    fn test_part2() {
+        let result = part2("data/example/day2.txt");
+
+        assert!(result.is_ok());
+        assert_eq!(result.unwrap(), 4);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 mod day1;
+mod day2;
 
 fn main() {
     println!("Day1 Part1: {:?}", day1::part1("data/input/day1.txt"));
     println!("Day1 Part2: {:?}", day1::part2("data/input/day1.txt"));
+
+    println!("Day2 Part1: {:?}", day2::part1("data/input/day2.txt"));
+    println!("Day2 Part2: {:?}", day2::part2("data/input/day2.txt"));
 }
